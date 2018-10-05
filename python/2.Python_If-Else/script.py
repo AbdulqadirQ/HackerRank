0c4749aa849dafed8645e236e0ea6adc30426e96
Abdulqadir Qasim <a.q.r@hotmail.co.uk>
2018-10-05 11:01:39 +0100

    2.if else task completion

---
+++ python/2.Python_If-Else/script.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+n = int(input())
+
+if n%2 is 1:
+    print("Weird")
+elif n >= 6 and n <= 20:
+    print("Weird")
+elif n >= 2 and n <= 5:
+    print("Not Weird")
+elif n > 20:
+    print ("Not Weird")
